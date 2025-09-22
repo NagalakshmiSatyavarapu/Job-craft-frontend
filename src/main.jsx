a90f7bad88f29b974e8f6aa401da70e0bf53c65e
NagalakshmiSatyavarapu <deepikasatyavarapu@gmail.com>
2025-09-22 22:48:31 +0530

    all the files are updated again

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // Ensure this path is correct
-import './index.css'; // Optional if you're using global styles
-
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/job-craft-frontend">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
