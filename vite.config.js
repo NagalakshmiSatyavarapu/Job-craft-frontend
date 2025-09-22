157cfbc1f4e50f58a0ef78c3024b1561680ede54
NagalakshmiSatyavarapu <deepikasatyavarapu@gmail.com>
2025-09-22 22:54:05 +0530

    Fix GitHub Pages paths for Job-craft-frontend

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/job-craft-frontend/',
+   base: '/Job-craft-frontend/',
   plugins: [react()],
  
 })
