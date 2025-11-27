527985ada19819c31fc7bc060de4a6d06cd7e844
2300030083 <2300030083@kluniversity.in>
2025-11-27 23:16:05 +0530

    Fix base path and deploy folder

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/job-craft-frontend/',   // 👈 EXACT repo name, with capital J
+  base: '/job-craft-frontend/',   // 👈 must be EXACT repo name on GitHub
 })
